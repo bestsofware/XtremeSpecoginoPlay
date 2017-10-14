17fd7e51aaef1640d2d7bd41afcc70734e17232c
bestsofware@github.com <bestsofware@users.noreply.github.com>
2017-10-14 19:10:33 +0200

    Ajout types entiers + texture erreur

---
+++ src/global.h
@@ -0,0 +1,35 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#ifdef DEBUG
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class dbg
+{
+public:
+	dbg() {}
+	~dbg() { std::cout << '\n'; }
+
+	template<class T>
+	std::ostream& operator<<(T& obj)
+	{
+		return std::cout << obj;
+	}
+};
+#else
+
+#endif
+
+//extern sf::RenderWindow* appPtr;
+
+typedef signed 		char 	int8;
+typedef unsigned 	char 	uint8;
+typedef signed 		short	int16;
+typedef unsigned 	short	uint16;
+typedef signed 		long	int32;
+typedef unsigned 	long	uint32;
+typedef signed   long long	int64;
+typedef unsigned long long	uint64;
+
+#endif
